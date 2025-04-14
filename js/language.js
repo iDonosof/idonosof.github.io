996f8279457ba6ada6b089fa63f498e739a622b1
Ignacio Donoso <naxonicolas1997@gmail.com>
2025-04-13 22:51:02 -0400

    WIP

--- js/language.js
+++ js/language.js
@@ -1,4 +1,5 @@
-let language = navigator.language.match(/es(-ES)?/) ? esES : enEN;
+const languageCode = new URLSearchParams(window.location.search).get("lang") || "es";
+const language = languageCode === "es" ? esES : enEN;
 
 document.querySelectorAll("*:not(html)[lang]").forEach(element => {
     const lang = element.getAttribute("lang");
--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
-{
-    document.querySelector("#age").innerHTML = getAge("1998-06-17");
-}
+const splide = new Splide(".menu-slide.splide", {
+    type: "loop",
+})
+    .on("move", function (newIndex) {
+        const element = document.querySelector(`#splide02-slide0${newIndex + 1}`);
+        SelectOption(newIndex);
+    })
+    .mount();
+
+new Splide(".splide.projects-slide", {
+    type: "loop",
+}).mount();
+
+document.querySelector("#age").innerHTML = getAge("1998-06-17");
 
 document.querySelectorAll("button.option").forEach((button) =>
     button.addEventListener("click", (e) => {
         const option = e.target.tagName === "SPAN" ? e.target.parentElement.getAttribute("value") : e.target.getAttribute("value");
-        document.querySelector(".menu-option.active").classList.remove("active");
-        document.querySelector(`.menu-option[menu-option="${option}"]`).classList.add("active");
-
-        //Update character image
-        //Update character background
+        SelectOption(parseInt(option));
     })
 );
+
+function SelectOption(optionValue) {
+    document.querySelector(".menu-option.active").classList.remove("active");
+    document.querySelector(`.menu-option[menu-option="${optionValue}"]`).classList.add("active");
+    splide.go(optionValue);
+    //Update character image
+    //Update character background
+}
