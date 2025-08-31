1420190e8c77c9e8163252e8ce4c18bd1735c51d
Ignacio Donoso <naxonicolas1997@gmail.com>
2025-08-30 23:36:34 -0400

    Add loading spinner and implement dynamic image loading for background carousel

--- js/index.js
+++ js/index.js
@@ -1,34 +1,80 @@
-// document.querySelector("#age").innerHTML = getAge("1998-06-17");
-
 const CHANGE_ANIMATION_INTERVAL = 10000;
 const FADE_IN_DURATION = 1000;
 const backgroundCarousel = new bootstrap.Carousel("#background-carousel");
 let touchXAxis = 0;
 
-window.addEventListener("load", () => {
+const images = [
+    {
+        name: "BackgroundNightJapan",
+        src: "img/backgrounds/BackgroundNightJapan.gif",
+    },
+    {
+        name: "MainCharacter",
+        src: "img/characters/MainCharacter.png",
+        hideSpinner: true,
+    },
+    {
+        name: "BackgroundRainStore",
+        src: "img/backgrounds/BackgroundRainStore.gif",
+    },
+    {
+        name: "MainCharacterBeach",
+        src: "img/characters/MainCharacterBeach.png",
+    },
+    {
+        name: "BackgroundRain",
+        src: "img/backgrounds/BackgroundRain.gif",
+    },
+    {
+        name: "MainCharacterSuit",
+        src: "img/characters/MainCharacterSuit.png",
+    },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    LoadNextImage(images.shift());
+    document.querySelector("#projects-section").addEventListener(
+        "touchstart",
+        (e) => {
+            touchXAxis = e.screenX;
+        },
+        false
+    );
+    document.querySelector("#projects-section").addEventListener(
+        "touchmove",
+        (e) => {
+            if (e.screenX < touchXAxis) {
+                new bootstrap.Carousel("#projects-carousel").next();
+            } else if (e.screenX > touchXAxis) {
+                new bootstrap.Carousel("#projects-carousel").prev();
+            }
+        },
+        false
+    );
+});
+
+function LoadNextImage(imageInfo) {
+    image = document.querySelector(`img[name="${imageInfo.name}"]`);
+    image.src = imageInfo.src;
+    image.onload = () => {
+        if (images.length > 0) {
+            if (imageInfo.hideSpinner) HideSpinner();
+            return LoadNextImage(images.shift());
+        }
+        StartBackgroundAnimation();
+    };
+}
+
+function HideSpinner() {
+    document.querySelector("#spinner").style.display = "none";
+    document.body.style.overflow = "auto";
+}
+
+function StartBackgroundAnimation() {
     setInterval(() => {
         ChangeAnimation();
     }, CHANGE_ANIMATION_INTERVAL);
-});
-
-document.querySelector("#projects-section").addEventListener(
-    "touchstart",
-    (e) => {
-        touchXAxis = e.screenX;
-    },
-    false
-);
-document.querySelector("#projects-section").addEventListener(
-    "touchmove",
-    (e) => {
-        if (e.screenX < touchXAxis) {
-            new bootstrap.Carousel("#projects-carousel").next();
-        } else if (e.screenX > touchXAxis) {
-            new bootstrap.Carousel("#projects-carousel").prev();
-        }
-    },
-    false
-);
+}
 
 async function ChangeAnimation() {
     const character = getActiveCharacter();
